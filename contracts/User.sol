ec57eba38ce309a13169828313af49ce461bb902
Chiro Hiro <chiro8x@gmail.com>
2021-05-29 15:42:49 +0700

    add registry user contract

---
+++ contracts/User.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: Apache-2.0
+
+pragma solidity >=0.8.4 <0.9.0;
+pragma abicoder v2;
+
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+import './Registry.sol';
+
+contract User {
+  // Registry contract
+  Registry internal registry;
+
+  // Active domain
+  bytes32 internal domain;
+
+  // Allow same domain calls
+  modifier onyAllowSameDomain(bytes32 name) {
+    require(msg.sender == registry.getAddress(domain, name), 'User: Only allow call from same domain');
+    _;
+  }
+
+  // Allow cross domain call
+  modifier onyAllowCrossDomain(bytes32 fromDomain, bytes32 name) {
+    require(msg.sender == registry.getAddress(fromDomain, name), 'User: Only allow call from allowed cross domain');
+    _;
+  }
+
+  // Constructing with registry address and its active domain
+  constructor(address _registry, bytes32 _domain) {
+    registry = Registry(_registry);
+    domain = _domain;
+  }
+
+  // Return active domain
+  function getDomain() external view returns (address) {
+    return domain;
+  }
+
+  // Return registry address
+  function getRegistry() external view returns (address) {
+    return address(registry);
+  }
+}
