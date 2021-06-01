dc6c3635a6c9c329ef4aa6144cde73baa112cb82
Chiro Hiro <chiro8x@gmail.com>
2021-06-01 10:15:07 +0700

    update nft token structure

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -4,18 +4,14 @@ pragma abicoder v2;
 
 import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
-import './Registry.sol';
-import './User.sol';
+import './libraries/User.sol';
+
 
 /**
  * Only allow Press to mint new token following domain
  * Domain: DKDAO Infrastructure
  */
-contract NFT is User, ERC721, Ownable {
-  // Constructing with registry address and domain
-  constructor(address _registry, bytes32 _domain) ERC721('DKDI', 'DKDAO Items') User(_registry, _domain) {
-  }
-
+contract NFT is User, ERC721('DKDI', 'DKDAO Items'), Ownable {
   // Only press able to mint new item
   function mint(address to, uint256 tokenId) external onlyAllowSameDomain(bytes32('Press')) returns (bool) {
     _mint(to, tokenId);
