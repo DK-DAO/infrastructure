07fc1ba4b066c1a4f1e7c9b1b0d6a32f13ee1439
Chiro Hiro <chiro8x@gmail.com>
2021-07-05 13:27:44 +0700

    add ability to delegate stake to another stake holder

--- contracts/DAOToken.sol
+++ contracts/DAOToken.sol
@@ -11,21 +11,33 @@ import './libraries/TokenMetadata.sol';
  * Domain: DKDAO, *
  */
 contract DAOToken is ERC20 {
-  // Lock structure
-  struct Lock {
+  // Staking structure
+  struct StakingData {
     uint256 amount;
     uint128 lockAt;
     uint128 unlockAt;
   }
 
-  // Mapping locked amount
-  mapping(address => Lock) private lockStorage;
+  // Main staking data storage
+  mapping(address => StakingData) stakingStorage;
 
-  // Lock event
-  event TokenLock(address indexed owner, uint256 indexed amount, uint256 lockTime);
+  // Delegate of stake
+  mapping(address => mapping(address => uint256)) delegation;
 
-  // Unlock event
-  event TokenUnlock(address indexed owner, uint256 indexed amount, uint256 indexed unlockTime);
+  // Voting power of an address
+  mapping(address => uint256) votePower;
+
+  // Staking event
+  event Staking(address indexed owner, uint256 indexed amount, uint256 indexed lockTime);
+
+  // Unstaking event
+  event Unstaking(address indexed owner, uint256 indexed amount, uint256 indexed unlockTime);
+
+  // Delegate event
+  event Delegate(address indexed owner, address indexed delegate, uint256 indexed amount);
+
+  // Stop delegation event
+  event StopDelegate(address indexed owner, address indexed delegate, uint256 indexed amount);
 
   // Constructing with token Metadata
   function init(TokenMetadata.Metadata memory metadata) external returns (bool) {
@@ -47,61 +59,105 @@ contract DAOToken is ERC20 {
     return true;
   }
 
-  // Overwrite {transferFrom} method
+  // Override {transferFrom} method
   function transferFrom(
     address sender,
     address recipient,
     uint256 amount
   ) public override returns (bool) {
+    // Make sure owner only able to transfer available token
     require(amount <= _available(sender), "DAOToken: You can't transfer larger than available amount");
-    _transfer(sender, recipient, amount);
-
     uint256 currentAllowance = allowance(sender, _msgSender());
-    require(currentAllowance >= amount, 'ERC20: transfer amount exceeds allowance');
-    _approve(sender, _msgSender(), currentAllowance - amount);
 
+    require(currentAllowance >= amount, 'ERC20: transfer amount exceeds allowance');
+    _transfer(sender, recipient, amount);
+    _approve(sender, _msgSender(), currentAllowance - amount);
     return true;
   }
 
-  // Overwrite {transfer} method
+  // Override {transfer} method
   function transfer(address recipient, uint256 amount) public override returns (bool) {
+    // Make sure owner only able to transfer available token
     require(amount <= _available(_msgSender()), "DAOToken: You can't transfer larger than available amount");
     _transfer(_msgSender(), recipient, amount);
     return true;
   }
 
-  // Lock an amount of token
-  function lock(uint256 amount) external returns (bool) {
+  // Staking an amount of token
+  function staking(uint256 amount, address delegate) external returns (bool) {
     address owner = _msgSender();
-    uint256 balance = balanceOf(owner);
-    require(amount > 0 && amount <= balance, "DAOToken: Lock amount can't be geater than your current balance");
-    // Store lock to storage
-    lockStorage[owner] = Lock({ amount: amount, lockAt: uint128(block.timestamp), unlockAt: 0 });
-    emit TokenLock(owner, amount, block.timestamp);
+    StakingData memory stakingData = stakingStorage[owner];
+    require(
+      amount <= _available(owner),
+      "DAOToken: Staking amount can't be geater than your current available balance"
+    );
+    // Increase staking amount
+    stakingData.amount += amount;
+    stakingData.lockAt = uint128(block.timestamp);
+    stakingData.unlockAt = 0;
+    // Increase vote power based on delegate status
+    if (delegate != address(0)) {
+      delegation[owner][delegate] = amount;
+      votePower[delegate] += amount;
+      // Fire deleaget event
+      emit Delegate(owner, delegate, amount);
+    } else {
+      votePower[owner] += amount;
+      // Fire self delegate event
+      emit Delegate(owner, owner, amount);
+    }
+    // Store staking to storage
+    stakingStorage[owner] = stakingData;
+    // Fire staking event
+    emit Staking(owner, amount, stakingData.lockAt);
     return true;
   }
 
-  // Unlock balance in the next 15 days
-  function unlock() external returns (bool) {
+  // Unstaking balance in the next 15 days
+  function unstaking() external returns (bool) {
     address owner = _msgSender();
-    Lock memory lockData = lockStorage[owner];
-    require(lockData.amount > 0, 'DAOToken: Unlock amount must greater than 0');
+    StakingData memory stakingData = stakingStorage[owner];
+    require(stakingData.amount > 0, 'DAOToken: Unstaking amount must greater than 0');
     // Set unlock time to next 15 days
-    lockData.unlockAt = uint128(block.timestamp + 15 days);
+    stakingData.unlockAt = uint128(block.timestamp + 15 days);
     // Set lock time to 0
-    lockData.lockAt = 0;
-    // Update lock data
-    lockStorage[owner] = lockData;
-    emit TokenUnlock(owner, lockData.amount, lockData.unlockAt);
+    stakingData.lockAt = 0;
+    // Update back data to staking storage
+    stakingStorage[owner] = stakingData;
+    // Fire unstaking event
+    emit Unstaking(owner, stakingData.amount, stakingData.unlockAt);
+    return true;
+  }
+
+  // Withdraw unstaked balance back to transferable balance
+  function withdraw(address[] memory delegates) external returns (bool) {
+    address owner = _msgSender();
+    StakingData memory stakingData = stakingStorage[owner];
+    // Temporary variable to redurce gas cost
+    uint256 ownerVotePower = votePower[owner];
+    uint256 delegatedPower;
+    // Make sure there are remaining staking balance and locking period was passed
+    require(stakingData.amount > 0, 'DAOToken: Unstaking amount must greater than 0');
+    require(block.timestamp > stakingData.unlockAt, 'DAOToken: The unlocking period was not passed');
+    // Return vote power from delegates to owner
+    for (uint256 i = 0; i < delegates.length; i += 1) {
+      delegatedPower = delegation[owner][delegates[i]];
+      ownerVotePower += delegatedPower;
+      emit StopDelegate(owner, delegates[i], delegatedPower);
+      delegation[owner][delegates[i]] = 0;
+    }
+    // Remove vote power from staking amount
+    stakingData.amount -= ownerVotePower;
+    // Reset vote power after remove from staking amount
+    votePower[owner] = 0;
+    // Update staking data
+    stakingStorage[owner] = stakingData;
     return true;
   }
 
   // Check transferable balance
   function _available(address owner) private view returns (uint256) {
-    if (isUnlocked(owner)) {
-      return balanceOf(owner);
-    }
-    return balanceOf(owner) - lockStorage[owner].amount;
+    return balanceOf(owner) - stakingStorage[owner].amount;
   }
 
   // Get balance info at once
@@ -111,30 +167,30 @@ contract DAOToken is ERC20 {
     returns (
       uint256 available,
       uint256 balance,
-      Lock memory unlockAt
+      StakingData memory stakingData
     )
   {
-    return (_available(owner), balanceOf(owner), lockStorage[owner]);
+    return (_available(owner), balanceOf(owner), stakingStorage[owner]);
   }
 
   // Calculate voting power
-  function votePower(address owner) external view returns (uint256) {
+  function calculatePower(address owner) external view returns (uint256) {
     // If token is locked and no unlocking
-    if (isLocked(owner)) {
-      return lockStorage[owner].amount;
+    if (isStaked(owner)) {
+      return votePower[owner];
     }
     return 0;
   }
 
   // Is completed unlock?
-  function isUnlocked(address owner) public view returns (bool) {
-    Lock memory lockData = lockStorage[owner];
+  function isUnstaked(address owner) public view returns (bool) {
+    StakingData memory stakingData = stakingStorage[owner];
     // Completed unlocked where they didn't lock or the unlocking period was over
-    return lockData.lockAt == 0 && (block.timestamp > lockData.unlockAt || lockData.amount == 0);
+    return stakingData.lockAt == 0 && (block.timestamp > stakingData.unlockAt || stakingData.amount == 0);
   }
 
   // Is completed lock?
-  function isLocked(address owner) public view returns (bool) {
-    return lockStorage[owner].lockAt > 0;
+  function isStaked(address owner) public view returns (bool) {
+    return stakingStorage[owner].lockAt > 0;
   }
 }
