62049a02f974f916f8636f67ccf26c147b9f0c53
Chiro Hiro <chiro8x@gmail.com>
2021-08-24 17:46:44 +0700

    add staking feature

--- contracts/infrastructure/Swap.sol
+++ contracts/infrastructure/Swap.sol
@@ -23,11 +23,20 @@ contract Swap is User, Ownable {
   // Nonce
   mapping(address => uint256) private nonceStorage;
 
+  // Staking
+  mapping(address => mapping(uint256 => uint256)) private stakingStorage;
+
   // List partner
-  event ListPartner(address partnerAddress);
+  event ListPartner(address indexed partnerAddress);
 
   // Delist partner
-  event DelistPartner(address partnerAddress);
+  event DelistPartner(address indexed partnerAddress);
+
+  // Start staking
+  event StartStaking(address indexed owner, uint256 indexed tokenId, uint256 timestamp);
+
+  // Stop staking
+  event StopStaking(address indexed owner, uint256 indexed tokenId, uint256 timestamp);
 
   // Only allow partner to execute
   modifier onlyPartner() {
@@ -56,6 +65,46 @@ contract Swap is User, Ownable {
     return INFT(getAddressSameDomain('NFT')).safeTransfer(_from, _to, _value);
   }
 
+  // Anyone could able to staking their good/items for their own sake
+  function startStaking(bytes memory tokenIds) external returns (bool) {
+    INFT nft = INFT(getAddressSameDomain('NFT'));
+    require(tokenIds.length % 32 == 0, 'Swap: Staking Ids need to be divied by 32');
+    for (uint256 i = 0; i < tokenIds.length; i += 32) {
+      uint256 tokenId = tokenIds.readUint256(i);
+      require(stakingStorage[msg.sender][tokenId] == 0, 'Swap: We are only able to stake, unstaked token');
+      // Store starting point of staking
+      stakingStorage[msg.sender][tokenId] = block.timestamp;
+      emit StartStaking(msg.sender, tokenId, block.timestamp);
+      require(
+        nft.safeTransfer(msg.sender, 0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa, tokenId),
+        'Swap: User should able to transfer their own token to staking address'
+      );
+    }
+    return true;
+  }
+
+  // Anyone could able to stop staking their good/items for their own sake
+  function stopStaking(bytes memory tokenIds) external returns (bool) {
+    INFT nft = INFT(getAddressSameDomain('NFT'));
+    require(tokenIds.length % 32 == 0, 'Swap: Staking Ids need to be divied by 32');
+    for (uint256 i = 0; i < tokenIds.length; i += 32) {
+      uint256 tokenId = tokenIds.readUint256(i);
+      require(
+        block.timestamp - stakingStorage[msg.sender][tokenId] > 1 days,
+        'Swap: You only able to stop staking after at least 1 day'
+      );
+      require(stakingStorage[msg.sender][tokenId] > 0, 'Swap: You only able to stop staking staked token');
+      // Reset staking state
+      stakingStorage[msg.sender][tokenId] = 0;
+      emit StopStaking(msg.sender, tokenId, block.timestamp);
+      require(
+        nft.safeTransfer(0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa, msg.sender, tokenId),
+        'Swap: User should able to transfer their own token to staking address'
+      );
+    }
+    return true;
+  }
+
   // List a new partner
   function listPartner(address partnerAddress) external onlyOwner returns (bool) {
     partners[partnerAddress] = true;
@@ -73,5 +122,20 @@ contract Swap is User, Ownable {
   // Get nonce of an address
   function getNonce(address owner) external view returns (uint256) {
     return nonceStorage[owner] + 1;
+  }
+
+  // Check an address is partner or not
+  function isPartner(address partner) external view returns (bool) {
+    return partners[partner];
+  }
+
+  // Get staking timestamp of given address and list of token Id
+  function getStakingStatus(address owner, bytes memory tokenIds) external view returns (uint256[] memory) {
+    require(tokenIds.length % 32 == 0, 'Swap: Staking Ids need to be divied by 32');
+    uint256[] memory result = new uint256[](tokenIds.length / 32);
+    for (uint256 i = 0; i < tokenIds.length; i += 1) {
+      result[i] = stakingStorage[owner][tokenIds.readUint256(i * 32)];
+    }
+    return result;
   }
 }
