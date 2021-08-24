d475c543f0161d5a1c4d689ef1930b9014dee461
Chiro Hiro <chiro8x@gmail.com>
2021-08-24 09:46:57 +0700

    fix proof

--- contracts/infrastructure/Swap.sol
+++ contracts/infrastructure/Swap.sol
@@ -43,17 +43,17 @@ contract Swap is User, Ownable {
   // User able to delegate transfer right with a cyrptographic proof
   function delegateTransfer(bytes memory proof) external onlyPartner returns (bool) {
     // Check for size of the proof
-    require(proof.length == 97, 'Swap: Wrong size of the proof, it must be 97 bytes');
+    require(proof.length == 149, 'Swap: Wrong size of the proof, it must be 149 bytes');
     bytes memory signature = proof.readBytes(0, 65);
     bytes memory message = proof.readBytes(65, proof.length - 65);
-    address _to = message.readAddress(0);
-    uint256 _tokenId = message.readUint256(20);
-    uint256 _nonce = message.readUint256(52);
     address _from = message.verifySerialized(signature);
+    address _to = message.readAddress(0);
+    uint256 _value = message.readUint256(20);
+    uint256 _nonce = message.readUint256(52);
     // Each nonce is only able to be use once
     require(_nonce - nonceStorage[_from] == 1, 'Swap: Incorrect nonce of signer');
     nonceStorage[_from] += 1;
-    return INFT(getAddressSameDomain('NFT')).safeTransfer(_from, _to, _tokenId);
+    return INFT(getAddressSameDomain('NFT')).safeTransfer(_from, _to, _value);
   }
 
   // List a new partner
