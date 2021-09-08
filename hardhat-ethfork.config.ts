ecd64d1483492c4e4b26282b279f948d50b59264
Chiro Hiro <chiro8x@gmail.com>
2021-09-08 12:05:15 +0700

    fixed a block number

--- hardhat-ethfork.config.ts
+++ hardhat-ethfork.config.ts
@@ -64,6 +64,7 @@ const config: HardhatUserConfig = {
       },
       forking: {
         url: env.DUELIST_KING_MAINNET_RPC,
+        blockNumber: 10626500,
         enabled: true,
       },
     },
