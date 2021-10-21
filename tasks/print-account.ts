dc3a8299cce16d8e3a74201dc309f5ba8fc9597f
Chiro Hiro <chiro8x@gmail.com>
2021-10-21 17:43:33 +0700

    Fix print accounts for better result

--- tasks/print-account.ts
+++ tasks/print-account.ts
@@ -6,10 +6,10 @@ import { HardhatRuntimeEnvironment } from 'hardhat/types';
 task('print:accounts', 'Deploy token and vesting contract').setAction(
   async (_taskArgs: any, hre: HardhatRuntimeEnvironment) => {
     const accounts = await hre.ethers.getSigners();
-    for (let i = 0; i < accounts.length; i += 1) {
+    for (let i = 0; i < 5; i += 1) {
       const account = accounts[i];
       console.log(
-        `${account.address}\t${(await account.getBalance()).div(10n ** 18n)} ${
+        `${account.address}\t${(await account.getBalance()).div(10n ** 14n).toNumber() / 10000} ${
           hre.network.name === 'binance' ? 'BNB' : 'ETH'
         }`,
       );
