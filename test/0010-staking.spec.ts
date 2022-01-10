1c684536234e0844bf0f8f8203dfc26883578fe8
kevin <dqtkien@gmail.coom>
2022-01-10 20:33:20 +0700

    Add more test case

--- test/0010-staking.spec.ts
+++ test/0010-staking.spec.ts
@@ -1,11 +1,14 @@
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
 import hre, { ethers } from 'hardhat';
-import { TestToken, MultiSig } from '../typechain';
+import { TestToken } from '../typechain';
+import { solidity } from 'ethereum-waffle';
 import Deployer from './helpers/deployer';
+
+chai.use(solidity);
 
 let stakingContract: any, contractTestToken: TestToken;
 
-describe.only('Staking', function () {
+describe('Staking', function () {
   it('Initialize', async function () {
     const deployer: Deployer = Deployer.getInstance(hre);
     const accounts = await ethers.getSigners();
@@ -15,6 +18,26 @@ describe.only('Staking', function () {
     stakingContract = await Staking.deploy(accounts[0].address);
     await stakingContract.deployed();
     expect(await stakingContract.getOwnerAddress()).to.equal(accounts[0].address);
+  });
+
+  it('should be failed when create a new campaign because token address is not a smart contract', async function () {
+    const blocktime = await stakingContract.getBlockTime();
+    const accounts = await ethers.getSigners();
+    const config = {
+      startDate: blocktime.add(Math.round(1 * 86400)),
+      endDate: blocktime.add(Math.round(30 * 86400)),
+      returnRate: 1,
+      maxAmountOfToken: 4000000,
+      stakedAmountOfToken: 0,
+      limitStakingAmountForUser: 500,
+      tokenAddress: accounts[1].address,
+      maxNumberOfBoxes: 32000,
+      numberOfLockDays: 15,
+    };
+
+    await expect(stakingContract.createNewStakingCampaign(config)).to.be.revertedWith(
+      'Staking: Token address is not a smart contract',
+    );
   });
 
   it('should return campaign something when created', async function () {
